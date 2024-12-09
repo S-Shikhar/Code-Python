ea5f6e5fefdb5b0b89eb92ef996fbf05f37d22b2
S-Shikhar <souhardashikharbiswas@gmail.com>
2024-12-09 11:13:02 +0530

    Refactor Test.py and update test.ipynb to improve code clarity and execution tracking

--- Practice/Test.py
+++ Practice/Test.py
@@ -20,7 +20,6 @@
 #     print("prime")
 
 
-
 fruits = ["mango", "apple", "banana", "grapes"]
 
 # print(len(fruits))
@@ -49,6 +48,6 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 num = num[::-1]
 
-print(num)
-for i in "hello":
-    print(i)
+# print(num)
+# for i in "hello":
+#     print(i)
