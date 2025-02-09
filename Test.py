4f1907760bb82e362d7b4aa150ef93651f73dc0e
SOUHARDA SHIKHAR BISWAS <souhardashikharbiswas@gmail.com>
2025-02-09 18:34:53 +0530

    Refactor Test.py to comment out existing code and add a new function for list updating

--- Test.py
+++ Test.py
@@ -1,21 +1,30 @@
-print("Souharda")
+# print("Souharda")
 
-a = "Souharda"
-print(type(a))
+# a = "Souharda"
+# print(type(a))
 
-name = input("Enter your name: ")
+# name = input("Enter your name: ")
 
-print(name)
+# print(name)
 
-if(name == "Souharda"):
-    print("My name is Souharda")
-else:
-    print("My name is not Souharda")
+# if(name == "Souharda"):
+#     print("My name is Souharda")
+# else:
+#     print("My name is not Souharda")
 
-name = input("Name: ")
-if(name == "Souharda"):
-	print("My name is Souharda")
-elif(name == "Souharda Shikhar Biswas"):
-	print("My name is Souharda Shikhar Biswas")
-else:
-	print("This is not my name")
+# name = input("Name: ")
+# if(name == "Souharda"):
+# 	print("My name is Souharda")
+# elif(name == "Souharda Shikhar Biswas"):
+# 	print("My name is Souharda Shikhar Biswas")
+# else:
+# 	print("This is not my name")
+
+def update_list():
+	l1[1:3] = l2[:2]
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [5, 6]
+
+update_list()
+print(l1)
